c955a9bd867bc830c6a338b082fc0a9e80e8f3e6
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-10-10 21:45:49 +0400

    1

---
+++ src/main/java/com/example/jd2_0/junittest/JUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jd2_0.junittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jd2_0/junittest/JUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jd2_0.junittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
